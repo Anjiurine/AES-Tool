b84710c676678cef3759e51e2887e315da6d0aac
Anjiurine <q78kg.1ffrj6@outlook.com>
2023-11-05 18:47:21 +0800

    add -key

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 import sys
 import os
-import shutil # 新增
+import shutil
 
 AES_BLOCK_SIZE = AES.block_size
 AES_KEY_SIZE = 16
-key = "ok let's go"
+try:
+    key = sys.argv[sys.argv.index('-key') + 1]
+except (IOError, ValueError):
+               print("Please specify the key")
+               sys.exit(1)
 
 def PadKey(key):
    if len(key) > AES_KEY_SIZE:
@@ -36,11 +40,10 @@ def AddSuffix(data, suffix):
    return data + base64.b64encode(suffix.encode()).decode('utf-8')
 
 def RemoveSuffix(data):
-   suffix_length = 8 # 你可以根据你的后缀长度来调整这个值
+   suffix_length = 8
    return data[:-suffix_length], base64.b64decode(data[-suffix_length:]).decode('utf-8')
 
 if __name__ == '__main__':
-   # 修改
    if len(sys.argv) < 3:
        print("Usage: python aes_encrypt.py <operation> [-dir <input_dir> -odir <output_dir>] [-input <input_file>]")
        sys.exit(1)
@@ -53,17 +56,16 @@ if __name__ == '__main__':
 
    key = PadKey(key.encode())
 
-   # 新增
-   if '-dir' in sys.argv and '-odir' in sys.argv: # 如果有-dir和-odir参数，表示加密或解密文件夹
-       input_dir = sys.argv[sys.argv.index('-dir') + 1] # 输入文件夹
-       output_dir = sys.argv[sys.argv.index('-odir') + 1] # 输出文件夹
+   if '-dir' in sys.argv and '-odir' in sys.argv:
+       input_dir = sys.argv[sys.argv.index('-dir') + 1]
+       output_dir = sys.argv[sys.argv.index('-odir') + 1]
        for root, dirs, files in os.walk(input_dir):
            for file in files:
-               input_file = os.path.join(root, file) # 输入文件的绝对路径
-               relative_path = os.path.relpath(input_file, input_dir) # 输入文件相对于输入文件夹的相对路径
-               output_file = os.path.join(output_dir, relative_path) # 输出文件的绝对路径
-               output_folder = os.path.dirname(output_file) # 输出文件所在的文件夹
-               if not os.path.exists(output_folder): # 如果输出文件夹不存在，就创建它
+               input_file = os.path.join(root, file)
+               relative_path = os.path.relpath(input_file, input_dir)
+               output_file = os.path.join(output_dir, relative_path)
+               output_folder = os.path.dirname(output_file)
+               if not os.path.exists(output_folder):
                    os.makedirs(output_folder)
                try:
                    with open(input_file, 'rb') as f:
@@ -74,14 +76,14 @@ if __name__ == '__main__':
 
                if operation == 'encrypt':
                    encodedData = Base64Encode(bytes)
-                   suffix = os.path.splitext(input_file)[1] # 获取文件的扩展名
+                   suffix = os.path.splitext(input_file)[1]
                    encodedData = AddSuffix(encodedData, suffix)
                    encryptedData = EnCrypt(key, encodedData.encode())
-                   output_file = output_file.replace(suffix, '.aes') # 直接修改后缀
+                   output_file = output_file.replace(suffix, '.aes')
                    try:
                        with open(output_file, 'wb') as f:
                            f.write(encryptedData)
-                       shutil.copyfile(output_file, output_folder) # 新增
+                       shutil.copyfile(output_file, output_folder)
                        print(f"Encrypted data written to {output_file}")
                    except IOError:
                        print(f"Output file {output_file} cannot be written.")
@@ -93,16 +95,16 @@ if __name__ == '__main__':
                        decryptData = DeCrypt(key, encryptedData)
                        decodedData, suffix = RemoveSuffix(decryptData.decode())
                        decodedData = Base64Decode(decodedData)
-                       output_file = output_file.replace('.aes', suffix) # 直接修改后缀
+                       output_file = output_file.replace('.aes', suffix)
                        with open(output_file, 'wb') as f:
                            f.write(decodedData)
-                       shutil.copyfile(output_file, output_folder) # 新增
+                       shutil.copyfile(output_file, output_folder)
                        print(f"Decrypted data written to {output_file}")
                    except (IOError, ValueError):
                        print(f"Input file {input_file} cannot be decrypted.")
                        continue
-   elif '-input' in sys.argv: # 如果有-input参数，表示加密或解密单个文件
-       input_file = sys.argv[sys.argv.index('-input') + 1] # 输入文件
+   elif '-input' in sys.argv:
+       input_file = sys.argv[sys.argv.index('-input') + 1]
        try:
            with open(input_file, 'rb') as f:
                bytes = f.read()
@@ -112,10 +114,10 @@ if __name__ == '__main__':
 
        if operation == 'encrypt':
            encodedData = Base64Encode(bytes)
-           suffix = os.path.splitext(input_file)[1] # 获取文件的扩展名
+           suffix = os.path.splitext(input_file)[1]
            encodedData = AddSuffix(encodedData, suffix)
            encryptedData = EnCrypt(key, encodedData.encode())
-           output_file = input_file.replace(suffix, '.aes') # 直接修改后缀
+           output_file = input_file.replace(suffix, '.aes')
            try:
                with open(output_file, 'wb') as f:
                    f.write(encryptedData)
@@ -130,13 +132,13 @@ if __name__ == '__main__':
                decryptData = DeCrypt(key, encryptedData)
                decodedData, suffix = RemoveSuffix(decryptData.decode())
                decodedData = Base64Decode(decodedData)
-               output_file = input_file.replace('.aes', suffix) # 直接修改后缀
+               output_file = input_file.replace('.aes', suffix)
                with open(output_file, 'wb') as f:
                    f.write(decodedData)
                print(f"Decrypted data written to {output_file}")
            except (IOError, ValueError):
                print(f"Input file {input_file} cannot be decrypted.")
                sys.exit(1)
-   else: # 如果没有-dir或-input参数，提示用户输入正确的用法
+   else:
        print("Please specify either -dir or -input.")
        sys.exit(1)
